d85a624e92f0bcb0515f87a207b754a074f8bfec
santokan <houska.peter@gmail.com>
2025-01-14 16:52:27 +0100

    base setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from graphics import Window, Line, Point
+
+
+def main():
+    win = Window(800, 600)
+    line = Line(Point(10, 10), Point(100, 100))
+    win.draw_line(line)
+    win.wait_for_close()
+
+
+main()
