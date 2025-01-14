4353845879eb66911cb57fa96360d118050fdc56
santokan <houska.peter@gmail.com>
2025-01-14 17:09:29 +0100

    added cell.py
    see TODO

---
+++ cell.py
@@ -0,0 +1,22 @@
+from graphics import Line, Point
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+
+        # TODO: if wall: draw line
